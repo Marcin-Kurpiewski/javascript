3dfa84669af5975193fa4af5e74e98246a65fc44
Marcin Kurpiewski <kurus.mk@gmail.com>
2016-05-18 23:57:26 +0200

    utworzylem 5 obiektow

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
 /**
  * Created by kurus.mk on 17.05.2016.
  */
-function sayHeloo() {
-    return "witaj, ";
-}
-var haj= sayHeloo();
+
+var audi ={ make: 'audi', model:'a5', color:'black', year:'2005', price:'12500', hp:'100',owners:'2'};
+var bmw ={ make:'bmw', model:'5', color:'red', year:'2010', price:'35000', hp:'125', owners:'3'};
+var fiat={ make:'fiat', model:'500', color:'white', year:'2016', price:'50000', hp:'65', owners:'4'};
+var ford={ make:'ford', model:'focus', color:'blue', year:'1999', price:'5450', hp:'99', owners:'5'};
+var honda={ make:'honda', model:'civic', color:'green', year:'2003', price:'9999', hp:'125', owners:'1'};
+
+
+
