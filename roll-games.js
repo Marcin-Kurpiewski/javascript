b0f8611fa7bae4852e589958844212f89819d9ba
Msrcin <kurus.mk@gmail.com>
2016-05-27 16:35:09 +0200

    kontener na gre,

--- roll-games.js
+++ roll-games.js
@@ -1,3 +1,24 @@
-/**
- * Created by kurus.mk on 25.05.2016.
- */
+ $(function() {
+        $( "#start" ).click(function() {
+            var B1, B2, B3;
+            B1=Math.round(Math.random()*10);
+            B2=Math.round(Math.random()*10);
+            B3=Math.round(Math.random()*10);
+
+            $( ".item-game-1" )
+                .animate({backgroundPositionY:0})
+                .animate({'background-position-y':3000+B1* 100},3000);
+            $( ".item-game-2" )
+                .animate({backgroundPositionY:0})
+                .animate({'background-position-y':3000+B2* 100},3000);
+            $( ".item-game-3" )
+                .animate({backgroundPositionY:0})
+                .animate({'background-position-y':3000+B3* 100},3000);
+setTimeout(function () {
+    checkResult(B1, B2, B3);
+}, 3500);
+
+        });
+    });
+
+
