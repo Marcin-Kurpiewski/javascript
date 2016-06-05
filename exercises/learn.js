2d79692718fbadeec8203f15ff8d9ac89584e70f
Marcin Kurpiewski <kurus.mk@gmail.com>
2016-06-05 23:45:37 +0200

    klasa, konstruktor ,

--- exercises/learn.js
+++ exercises/learn.js
@@ -7,22 +7,35 @@ var b= 3;
 var c = a + b;
 
 console.log(c*a);
+var arr= new Array();
 
 
-function newCars (model, color, speed, year){
-    this.model=model;
-    this.color=color;
-    this.speed=speed;
-    this.year=year;
+// klasa na podstawie której tworzymy obiekty
+function NewCar (model, color, speed, year){
+    this.carModel=model;
+    this.carColor=color;
+    this.carSpeed=speed;
+    this.carYear=year;
 }
 
-var fiat= new newCars('500','blue',180,1985);
-var bmw= new newCars('535','red',220,2003);
-var volvo= new newCars('s60','black',199,2000);
-var volkswagen= new newCars('golf','green', 175,1999);
-var audi= new newCars('a6','silver', 235, 2016);
 
-var allCars = [fiat, volkswagen, bmw, volvo,audi];
+NewCar.prototype.showColor= function(){
+    return 'car color is ' + this.carColor;
+};
+
+
+// na podstawie klasy tworzymy  nowa instacje - obiekt.
+//
+// konstruktor wywoluje sie wtedy kiedy tworzona jest nowa instancja
+var fiat = new NewCar('500','blue',180,1985);
+var bmw= new NewCar('535','red',220,2003);
+var volvo= new NewCar('s60','black',199,2000);
+var volkswagen=new NewCar('golf','green', 175,1999);
+var audi=new NewCar('a6','silver', 235, 2016);
+
+
+
+var garage=[{fiat,bmw, volvo, volkswagen, audi}];
 
 
 
